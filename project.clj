51acac29733daf0d2b994e98787b5e312f8d5947
Tom Goldsmith <goldsmith.tee@gmail.com>
2017-02-23 20:52:13 -0500

    Clean up project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject word-trek-solver "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A fast solver for games in the Boggle family, e.g. WordBrain, Work
+Trek Trek, etc. Given a puzzle to solve, this library will compute all
+possible words that can be spelled (i.e. it can provide word hints)."
+  :url "https://github.com/tomthought/word-trek-solver"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
